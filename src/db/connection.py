be03acb65f07751ced85dfa2bac8ebd7f81a96be
davidseres <davidseres@gmail.com>
2023-06-11 19:40:03 +0200

    updated connection.py

--- src/db/connection.py
+++ src/db/connection.py
@@ -13,9 +13,9 @@ load_dotenv()
 
 # Connect to the PostgreSQL database
 conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
-    database=os.getenv("DB_DATABASE"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    port=os.getenv("DB_PORT")
+    host=os.environ.get("DB_HOST"),
+    database=os.environ.get("DB_DATABASE"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASSWORD"),
+    port=os.environ.get("DB_PORT")
 )
